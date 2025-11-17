19cd189f5c5f221b098cafe51e6aba06ac8bfc99
@CrazyVaibhav20 <155980475+yellowmonsvaibhav@users.noreply.github.com>
2025-11-17 18:39:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+export const CLIENT_ID = "Ov23lihC3y93z52VK93G";
+export const BACKEND_URL = "http://localhost:3000";
---
+++ supabaseClient.js
@@ -0,0 +1,10 @@
+
+
+
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "";
+const SUPABASE_KEY = "I";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
