97410678441738c6743042621e6aa38fdd5246cd
@CrazyVaibhav20 <155980475+yellowmonsvaibhav@users.noreply.github.com>
2025-12-30 13:29:49 +0530

    Update CLIENT_ID and reset BACKEND_URL

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = "Ov23lihC3y93z52VK93G";
-export const BACKEND_URL = "http://localhost:3000";
+export const CLIENT_ID = "Ov23liIl49Xxvjbh1FEn";
+export const BACKEND_URL = "";
